10b1abf442de62cfebbae2d64e54481739124c04
Nick Pan <nicholas.pan1@gmail.com>
2018-03-13 20:20:09 -0400

    Restart demo to Hello World

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import (
 )
 
 func hello(w http.ResponseWriter, r *http.Request) {
-	io.WriteString(w, "Hello CBS, we've deployed to Kubernetes")
+	io.WriteString(w, "Hello World!")
 }
 
 func main() {
