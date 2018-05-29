87a54e094312ac2338b1aa8e1651d40be3ea133e
Nick Pan <nicholas.pan1@gmail.com>
2018-05-29 16:41:23 -0400

    Add color to the page

--- main.go
+++ main.go
@@ -2,14 +2,27 @@ package main
 
 import (
 	"io"
+	"io/ioutil"
+	"os"
+	"fmt"
 	"net/http"
 )
 
-func hello(w http.ResponseWriter, r *http.Request) {
-	io.WriteString(w, "Hello World!")
+func index(w http.ResponseWriter, r *http.Request) {
+	fmt.Printf("Handling %+v\n", r);
+	bs, err := ioutil.ReadFile("/content/index.html")
+
+	if err != nil {
+		fmt.Printf("Couldn't read index.html: %v", err);
+		os.Exit(1)
+	}
+
+	io.WriteString(w, string(bs[:]))
 }
 
 func main() {
-	http.HandleFunc("/", hello)
-	http.ListenAndServe(":80", nil)
+	http.HandleFunc("/", index)
+	port := ":8000"
+	fmt.Printf("Starting to service on port %s\n", port);
+	http.ListenAndServe(port, nil)
 }
